a92f4c4a4d91f3d384204740504995db9b935d7a
Ambitious <153156513+AmbitiousPhantom@users.noreply.github.com>
2025-03-10 23:58:29 -0700

    Update Login.js

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput} from 'react-native'; 
+import Colors from '../constants/Colors';
 const Login = () => {
     const [text, setText] = useState('');
     return(
-        <View>
-            <Text>Login</Text>
+        <View style={{flex:1, backgroundColor: Colors.primary}}>
+        <View style={{alignItems: "center", justifyContent: "center", transform:[{translateX:'0%'},{translateY:'30%'}]}}>
+            <Text style={{fontWeight: 400, fontSize: 30}}>Login</Text>
             <Text>Email Address</Text>
             <TextInput style={{height: 40, padding: 5}}
             placeholder='Email'
@@ -12,7 +14,13 @@ const Login = () => {
             defaultValue={text}
             />
             <Text>Password</Text>
-            <Text>Click this box to remember sign in details</Text>
+            <TextInput style={{height: 40, padding: 5}}
+            placeholder='Password'
+            onChangeText={newText => setText(newText)}
+            defaultValue={text}
+            />
+            <Text style={{color:Colors.secondary}}>Click this box to remember sign in details</Text>
+            </View>
         </View>
     )
 }
