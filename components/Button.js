36bae305658931f9bb5301dc7d754b5c454c3257
Ambitious <153156513+AmbitiousPhantom@users.noreply.github.com>
2025-03-13 21:08:42 -0700

    Add files via upload

---
+++ components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, StyleSheet,TouchableOpacity} from 'react-native';
+import Colors from '../constants/Colors';
+
+const Button = () => {
+
+    return (
+        <View></View>
+    )
+}
+
+const styles = StyleSheet.create({
+    styles:{
+        
+    },
+})
+
+export default Button;
---
+++ components/Touchables.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View} from "react-native";
+const Touchables = () => {
+
+    return(
+    <View></View>
+    )
+};
+
+export default Touchables;
